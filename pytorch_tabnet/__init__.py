04f2f772858d5b4225b8a867a21337abea19d487
Marcel Falkiewicz <mfalkiewicz@gmail.com>
2024-12-10 22:28:39 +0100

    Minor additions

--- pytorch_tabnet/__init__.py
+++ pytorch_tabnet/__init__.py
@@ -10,6 +10,3 @@ from .tab_model import TabNetRegressor, TabNetClassifier
 from .tab_network import TabNet
 
 __all__ = ["TabNetRegressor", "TabNetClassifier", "TabNet", "__version__"]
-from . import _version
-
-__version__ = _version.get_versions()["version"]
